e417adae99023ef34ec7975922d37707436839ab
GiovaLuc <gldacierno@gmail.com>
2024-11-22 16:10:44 +0100

    RestituisciLibro

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class Biblioteca {
-    
+    private ArrayList<Utente> utenti = new ArrayList();
+    private ArrayList<Utente> libri = new ArrayList();
+
+    public void restituisciLibro(String titolo){
+        for(Libro libro : libri){
+            if(libro.getTitolo.equals(titolo.toLowerCase())){
+                libro.restituisci();
+            }
+        }
+    }
+
 }
+
+
